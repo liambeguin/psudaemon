599302941d3dc1fe42f849fe9655b25d1d4b9185
Liam Beguin <liambeguin@gmail.com>
2023-07-27 12:31:56 -0400

    routers: add basic skeleton
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- psudaemon/app.py
+++ psudaemon/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 
+from .routers import monitoring
+
 
 def main():
     app = FastAPI()
+    app.include_router(monitoring.router)
+
     uvicorn.run(app, host='0.0.0.0', port=5000, log_level="info")
