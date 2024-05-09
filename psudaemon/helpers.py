35d182d8c9b280747c1e02862de0450e22679ed8
Liam Beguin <liambeguin@gmail.com>
2024-05-09 16:12:40 -0400

    psu: make sure API uses proper channel index
    the e36300 series has an offset between the channel list and the channel
    index. This stems from the fact that we don't have CH0.
    
    Make sure we always use the proper index.
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- psudaemon/helpers.py
+++ psudaemon/helpers.py
@@ -19,8 +19,14 @@ def check_user_input(
 
     psu = units[psu]
 
-    if ch is not None:
-        ch = psu.channels[ch]
-        ch.psu_name = psu.name
+    if ch is None:
+        return psu, None
 
-    return psu, ch
+    for chan in psu.channels:
+        if ch == chan.index:
+            ch = chan
+            ch.psu_name = psu.name
+            return psu, ch
+
+    # should never reach this
+    raise HTTPException(status_code=404, detail=f'channel out of range: {ch} not in range({len(units[psu].channels)})')
