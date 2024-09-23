79d239622ade8770e0d23845d5b00176086bfcc3
Liam Beguin <liambeguin@gmail.com>
2024-09-23 11:18:24 -0400

    context: update conffile handling
    In preparation for the addition of write support, make conffile into a
    class.
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- psudaemon/context.py
+++ psudaemon/context.py
@@ -17,25 +17,27 @@ class Settings(BaseModel):
     uvicorn: Dict[str, Any] = {}
 
 
-def _read_conffile() -> dict:
-    yaml = YAML()
-    conf = Path().parent / 'conf/psudaemon.yaml'
-    path = Path(os.getenv('PSUDAEMON_CONF', conf.as_posix()))
+class Conffile():
+    def __init__(self):
+        self.yaml = YAML()
+        conf = Path().parent / 'conf/psudaemon.yaml'
+        self.path = Path(os.getenv('PSUDAEMON_CONF', conf.as_posix()))
 
-    with path.open(encoding='utf-8') as f:
-        return yaml.load(f.read())
+    def read(self) -> dict:
+        with self.path.open(encoding='utf-8') as f:
+            return self.yaml.load(f.read())
 
 
 def load_settings() -> Settings:
     '''Load settings from file.'''
-    data = _read_conffile()
+    data = Conffile().read()
     return Settings(**data['settings'])
 
 
 @lru_cache
 def load_units() -> Dict[str, psumodels.Unit]:
     '''Load unit configuration from file.'''
-    data = _read_conffile()
+    data = Conffile().read()
 
     # rekey for easy access
     units = {k['name']: k for k in data['units']}
